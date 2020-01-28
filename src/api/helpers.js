8ddd1658dc38ab95a5caef4587de8f028c9f030e
chunfan song <songcf@chunfandeMacBook-Pro.local>
2020-01-28 18:55:00 +0800

    mk

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const urlMap = {
   development: '/',
-  production: 'http://pennycy.com/sell'
+  production: 'https://pennycy.com/sell'
 }
 const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
